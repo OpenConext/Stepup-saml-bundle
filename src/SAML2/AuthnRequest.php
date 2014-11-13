c3655d528f969f9b0e21351ea70792e70ea62268
DRvanR <dvrenterghem@ibuildings.nl>
2014-11-13 11:24:02 +0100

    Allow setting and getting of the AuthnContextClassReferences in SAMLRequests

--- src/SAML2/AuthnRequest.php
+++ src/SAML2/AuthnRequest.php
@@ -87,6 +87,28 @@ class AuthnRequest
     }
 
     /**
+     * @param array $requestedAuthnClassRef
+     */
+    public function setRequestedAuthenticationContext(array $requestedAuthnClassRef)
+    {
+        $authnContext = ['AuthnContextClassRef' => $requestedAuthnClassRef];
+        $this->request->setRequestedAuthnContext($authnContext);
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getRequestedAuthenticationContext()
+    {
+        $authnContext = $this->request->getRequestedAuthnContext();
+        if (!is_array($authnContext) || !array_key_exists('AuthnContextClassRef', $authnContext)) {
+            return null;
+        }
+
+        return $authnContext['AuthnContextClassRef'];
+    }
+
+    /**
      * @return string
      */
     public function getRequestId()
