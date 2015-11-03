0e1738d6e9912c95b988d727666a2d669ddff30a
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-03 13:57:29 +0100

    Updates tests to be more precise, add AttributeDictionaryTest

--- src/Tests/SAML2/Response/Assertion/AssertionAdapterTest.php
+++ src/Tests/SAML2/Response/Assertion/AssertionAdapterTest.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Copyright 2015 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace SAML2\Response\Assertion;
 
 use Mockery as m;
@@ -15,74 +31,130 @@ class AssertionAdapterTest extends TestCase
      * @test
      * @group AssertionAdapter
      */
-    public function create_empty_attribute_list_if_no_definition_for_attribute_found()
+    public function presence_of_attribute_can_be_confirmed_based_on_mace_urn_attribute_definition()
     {
+
+        $maceAttributeUrn   = 'urn:mace:some:attribute';
+        $maceAttributeValue = ['mace-attribute-value'];
+        $existingMaceAttributeDefinition = new AttributeDefinition(
+            'existingMaceAttribute',
+            $maceAttributeUrn,
+            'urn:oid:0.0.0.0.0.0.0.0.0'
+        );
+
         $assertion = m::mock('\SAML2_Assertion');
-        $assertion->shouldReceive('getAttributes')
-            ->andReturn([
-                'urn:mace:some:attribute' => ['some-value'],
-                'urn:oid:0.0.0.0.0.0.0.0.1' => ['another-value']
-            ]);
+        $assertion->shouldReceive('getAttributes')->andReturn([$maceAttributeUrn => $maceAttributeValue]);
 
         $dictionary = new AttributeDictionary();
+        $dictionary->addAttributeDefinition($existingMaceAttributeDefinition);
 
-        $adapter = new AssertionAdapter($assertion, $dictionary);
+        $attributeExpectedToBeContained = new Attribute($existingMaceAttributeDefinition, $maceAttributeValue);
+
+        $adapter      = new AssertionAdapter($assertion, $dictionary);
         $attributeSet = $adapter->getAttributeSet();
+        $attributeIsInSet = $attributeSet->contains($attributeExpectedToBeContained);
 
-        $this->assertCount(0, $attributeSet);
+        $this->assertTrue($attributeIsInSet, 'Expected attribute to be part of AttributeSet, but it is not');
     }
 
     /**
      * @test
      * @group AssertionAdapter
      */
-    public function create_attribute_list_based_on_assertion_and_attribute_definitions()
+    public function presence_of_attribute_can_be_confirmed_based_on_oid_urn_attribute_definition()
     {
+        $oidAttributeUrn   = 'urn:oid:0.0.0.0.0.0.0.0.0';
+        $oidAttributeValue = ['oid-attribute-value'];
+        $existingOidAttributeDefinition = new AttributeDefinition(
+            'existingOidAttribute',
+            $oidAttributeUrn,
+            'urn:oid:0.0.0.0.0.0.0.0.0'
+        );
+
         $assertion = m::mock('\SAML2_Assertion');
-        $assertion->shouldReceive('getAttributes')
-            ->andReturn([
-                'urn:mace:some:attribute' => ['some-value'],
-                'urn:oid:0.0.0.0.0.0.0.0.1' => ['another-value']
-            ]);
+        $assertion->shouldReceive('getAttributes')->andReturn([$oidAttributeUrn => $oidAttributeValue]);
+
+        $dictionary = new AttributeDictionary();
+        $dictionary->addAttributeDefinition($existingOidAttributeDefinition);
+
+        $attributeExpectedToBeContained = new Attribute($existingOidAttributeDefinition, $oidAttributeValue);
+
+        $adapter      = new AssertionAdapter($assertion, $dictionary);
+        $attributeSet = $adapter->getAttributeSet();
+        $attributeIsInSet = $attributeSet->contains($attributeExpectedToBeContained);
+
+        $this->assertTrue($attributeIsInSet, 'Expected attribute to be part of AttributeSet, but it is not');
+    }
+
+    /**
+     * @test
+     * @group AssertionAdapter
+     */
+    public function no_presence_of_attribute_can_be_confirmed_if_no_attribute_definition_found()
+    {
+        $oidAttributeUrn   = 'urn:oid:0.0.0.0.0.0.0.0.0';
+        $oidAttributeValue = ['oid-attribute-value'];
+        $existingOidAttributeDefinition = new AttributeDefinition(
+            'existingOidAttribute',
+            $oidAttributeUrn,
+            'urn:oid:0.0.0.0.0.0.0.0.0'
+        );
+
+        $assertion = m::mock('\SAML2_Assertion');
+        $assertion->shouldReceive('getAttributes')->andReturn([$oidAttributeUrn => $oidAttributeValue]);
 
         $dictionary = new AttributeDictionary();
 
-        $dictionary->addAttributeDefinition(new AttributeDefinition(
-            'someAttribute',
-            'urn:mace:some:attribute',
-            'urn:oid:0.0.0.0.0.0.0.0.0'
-        ));
+        $attributeExpectedNotToBeContained = new Attribute($existingOidAttributeDefinition, $oidAttributeValue);
 
-        $dictionary->addAttributeDefinition(new AttributeDefinition(
-            'anotherAttribute',
-            'urn:mace:another:attribute',
-            'urn:oid:0.0.0.0.0.0.0.0.1'
-        ));
-
-        $adapter = new AssertionAdapter($assertion, $dictionary);
-
+        $adapter      = new AssertionAdapter($assertion, $dictionary);
         $attributeSet = $adapter->getAttributeSet();
 
-        $containsSomeAttribute = $attributeSet->contains(new Attribute(
-            new AttributeDefinition(
-                'someAttribute',
-                'urn:mace:some:attribute',
-                'urn:oid:0.0.0.0.0.0.0.0.0'
-            ),
-            ['some-value']
-        ));
+        $attributeIsNotInSet = !$attributeSet->contains($attributeExpectedNotToBeContained);
 
-        $containsAnotherAttribute = $attributeSet->contains(new Attribute(
-            new AttributeDefinition(
-                'anotherAttribute',
-                'urn:mace:another:attribute',
-                'urn:oid:0.0.0.0.0.0.0.0.1'
-            ),
-            ['another-value']
-        ));
+        $this->assertTrue($attributeIsNotInSet, 'Expected attribute to not be part of AttributeSet, but it is');
+    }
 
-        $this->assertCount(2, $attributeSet);
-        $this->assertTrue($containsSomeAttribute);
-        $this->assertTrue($containsAnotherAttribute);
+    /**
+     * @test
+     * @group AssertionAdapter
+     */
+    public function attribute_list_is_empty_if_no_attributes_found()
+    {
+        $assertion = m::mock('\SAML2_Assertion');
+        $assertion->shouldReceive('getAttributes')->andReturn([]);
+
+        $dictionary = new AttributeDictionary();
+
+        $adapter      = new AssertionAdapter($assertion, $dictionary);
+        $attributeSet = $adapter->getAttributeSet();
+
+        $this->assertCount(0, $attributeSet, 'Expected attribute set to be empty, but it is not');
+    }
+
+    /**
+     * @test
+     * @group AssertionAdapter
+     */
+    public function attribute_list_has_content_when_attributes_found()
+    {
+        $oidAttributeUrn   = 'urn:oid:0.0.0.0.0.0.0.0.0';
+        $oidAttributeValue = ['oid-attribute-value'];
+        $existingOidAttributeDefinition = new AttributeDefinition(
+            'existingOidAttribute',
+            $oidAttributeUrn,
+            'urn:oid:0.0.0.0.0.0.0.0.0'
+        );
+
+        $assertion = m::mock('\SAML2_Assertion');
+        $assertion->shouldReceive('getAttributes')->andReturn([$oidAttributeUrn => $oidAttributeValue]);
+
+        $dictionary = new AttributeDictionary();
+        $dictionary->addAttributeDefinition($existingOidAttributeDefinition);
+
+        $adapter      = new AssertionAdapter($assertion, $dictionary);
+        $attributeSet = $adapter->getAttributeSet();
+
+        $this->assertCount(1, $attributeSet, 'Expected attribute AttributeSet to have content, but it does not');
     }
 }
