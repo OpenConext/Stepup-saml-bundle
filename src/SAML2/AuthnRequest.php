ad104ce13139cdfe9c1437245f422b25d3c04ca7
Elze Kool <e.kool@youwe.nl>
2016-03-25 20:14:35 +0100

    Added isPassive() and isForceAuthn() to AuthnRequest

--- src/SAML2/AuthnRequest.php
+++ src/SAML2/AuthnRequest.php
@@ -170,6 +170,22 @@ class AuthnRequest
     /**
      * @return bool
      */
+    public function isPassive()
+    {
+       return $this->request->getIsPassive();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isForceAuthn()
+    {
+       return $this->request->getForceAuthn();
+    }
+
+    /**
+     * @return bool
+     */
     public function isSigned()
     {
         return isset($this->signature);
