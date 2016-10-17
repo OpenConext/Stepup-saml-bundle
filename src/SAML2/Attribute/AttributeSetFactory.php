135d7b866a5fb09c3575f1c7274f4a18e6de59fb
Alex Rothuis <arothuis@ibuildings.nl>
2016-10-17 16:56:33 +0200

    Extract interface for attribute set factories

---
+++ src/SAML2/Attribute/AttributeSetFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright 2016 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\SamlBundle\SAML2\Attribute;
+
+use SAML2_Assertion;
+
+interface AttributeSetFactory
+{
+    /**
+     * @param SAML2_Assertion $assertion
+     * @param AttributeDictionary $attributeDictionary
+     * @return AttributeSet
+     *
+     * @deprecated Will be replaced with different creation implementation
+     */
+    public static function createFrom(SAML2_Assertion $assertion, AttributeDictionary $attributeDictionary);
+
+    /**
+     * @param Attribute[] $attributes
+     * @return AttributeSet
+     *
+     * @deprecated Will be replaced with different creation implementation
+     */
+    public static function create(array $attributes);
+}
