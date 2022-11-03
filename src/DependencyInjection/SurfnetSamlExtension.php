75710c56c920e1c59e0508a5db154d112c10df83
wdttilburg <info@webdesigntilburg.nl>
2022-11-03 15:35:58 +0100

    Update SurfnetSamlExtension.php

--- src/DependencyInjection/SurfnetSamlExtension.php
+++ src/DependencyInjection/SurfnetSamlExtension.php
@@ -38,8 +38,8 @@ class SurfnetSamlExtension extends Extension
         $config        = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-        $loader->load('saml_attributes.yml');
+        $loader->load('services.yaml');
+        $loader->load('saml_attributes.yaml');
 
         $this->parseHostedConfiguration($config['hosted'], $container);
         $this->parseRemoteConfiguration($config['remote'], $container);
