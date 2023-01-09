50424e38bb037a845aaef4e43f0c5da722412ae9
Michiel Kodde <mkodde@ibuildings.nl>
2023-01-09 14:11:41 +0100

    Add a SAML Authenticator
    This authenticator is compatible with the Symfony Security system. It
    creates a custom Authenticator class. The user must integrate this
    authenticator in the application. And a custom user provider must be
    created as this ties into the apolication logic to strongly to
    facilitate this in the bundle.
    
    Examples where added to EXAMPLES.md and README.md

---
+++ src/Security/Authentication/SamlInteractionProvider.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright 2023 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\SamlBundle\Security\Authentication;
+
+use SAML2\Assertion;
+use Surfnet\SamlBundle\Entity\IdentityProvider;
+use Surfnet\SamlBundle\Entity\ServiceProvider;
+use Surfnet\SamlBundle\Http\PostBinding;
+use Surfnet\SamlBundle\Http\RedirectBinding;
+use Surfnet\SamlBundle\SAML2\AuthnRequestFactory;
+use Surfnet\SamlBundle\Security\Exception\UnexpectedIssuerException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class SamlInteractionProvider
+{
+    public function __construct(
+        private readonly ServiceProvider $serviceProvider,
+        private readonly IdentityProvider $identityProvider,
+        private readonly RedirectBinding $redirectBinding,
+        private readonly PostBinding $postBinding,
+        private readonly SamlAuthenticationStateHandler $samlAuthenticationStateHandler
+    ) {
+    }
+
+    public function isSamlAuthenticationInitiated(): bool
+    {
+        return $this->samlAuthenticationStateHandler->hasRequestId();
+    }
+
+    public function initiateSamlRequest(): RedirectResponse
+    {
+        $authnRequest = AuthnRequestFactory::createNewRequest(
+            $this->serviceProvider,
+            $this->identityProvider
+        );
+
+        $this->samlAuthenticationStateHandler->setRequestId($authnRequest->getRequestId());
+
+        return $this->redirectBinding->createResponseFor($authnRequest);
+    }
+
+    public function processSamlResponse(Request $request): Assertion
+    {
+        $assertion = $this->postBinding->processResponse(
+            $request,
+            $this->identityProvider,
+            $this->serviceProvider
+        );
+
+        if ($assertion->getIssuer()->getValue() !== $this->identityProvider->getEntityId()) {
+            throw new UnexpectedIssuerException(sprintf(
+                'Expected issuer to be configured remote IdP "%s", got "%s"',
+                $this->identityProvider->getEntityId(),
+                $assertion->getIssuer()->getValue()
+            ));
+        }
+        return $assertion;
+    }
+
+    /**
+     * Resets the SAML flow.
+     */
+    public function reset(): void
+    {
+        $this->samlAuthenticationStateHandler->clearRequestId();
+    }
+}
